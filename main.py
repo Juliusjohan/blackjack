c27df5d330887810a4095ad92cd624d0f0866bcf
Johan Johansson <Juliusjohan@gmail.com>
2022-09-20 11:40:39 +0200

    change main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from player import Player
-
-STARTING_BALANCE = 500
-
-player_name = str(input("Enter your name: "))
-p1 = Player(player_name, STARTING_BALANCE)
-print(f"Hello {player_name}, you will start with ${STARTING_BALANCE}.")
