806b069c2b60a004c4d441019a0364cebef604a6
Johan Johansson <Juliusjohan@gmail.com>
2022-09-20 11:38:33 +0200

    Add player.py

---
+++ player.py
@@ -0,0 +1,10 @@
+"""
+This file will be used to generate the player and save the following data:
+Name of the player
+The balance of the player
+"""
+
+class Player():
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
